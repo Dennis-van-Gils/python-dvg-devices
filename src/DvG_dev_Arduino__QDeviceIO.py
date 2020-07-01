77063835c91c17382c6d123201153e18016529d3
Dennis-van-Gils <vangils.dennis@gmail.com>
2020-07-01 18:31:59 +0200

    Reset versioning

--- src/DvG_dev_Arduino__QDeviceIO.py
+++ src/DvG_dev_Arduino__QDeviceIO.py
@@ -3,93 +3,94 @@
 """PyQt5 module to provide multithreaded communication and periodical data
 acquisition for an Arduino(-like) device.
 """
-__author__      = "Dennis van Gils"
+__author__ = "Dennis van Gils"
 __authoremail__ = "vangils.dennis@gmail.com"
-__url__         = "https://github.com/Dennis-van-Gils/DvG_dev_Arduino"
-__date__        = "11-12-2018"
-__version__     = "1.2.0"
+__url__ = "https://github.com/Dennis-van-Gils/python-dvg-devices"
+__date__ = "01-07-2020"  # 0.0.1 was stamped 11-12-2018
+__version__ = "0.0.1"  # 0.0.1 corresponds to prototype 1.2.0
 
 from PyQt5 import QtCore
-import DvG_dev_Arduino__fun_serial as Arduino_functions
-import DvG_dev_Base__pyqt_lib      as Dev_Base_pyqt_lib
+import DvG_dev_Arduino__proto_serial
+from DvG_QDeviceIO import QDeviceIO, DAQ_trigger
 
 # Show debug info in terminal? Warning: Slow! Do not leave on unintentionally.
-DEBUG_worker_DAQ  = False
-DEBUG_worker_send = False
+DEBUG_worker_DAQ = False
+DEBUG_worker_jobs = False
 
 # ------------------------------------------------------------------------------
 #   Arduino_pyqt
 # ------------------------------------------------------------------------------
 
-class Arduino_pyqt(Dev_Base_pyqt_lib.Dev_Base_pyqt, QtCore.QObject):
+
+class Arduino_pyqt(QDeviceIO, QtCore.QObject):
     """Manages multithreaded communication and periodical data acquisition for
     an Arduino(-like) device.
 
-    All device I/O operations will be offloaded to 'workers', each running in 
+    All device I/O operations will be offloaded to 'workers', each running in
     a newly created thread instead of in the main/GUI thread.
-        
+
         - Worker_DAQ:
             Periodically acquires data from the device.
 
-        - Worker_send:
+        - Worker_jobs:
             Maintains a thread-safe queue where desired device I/O operations
             can be put onto, and sends the queued operations first in first out
             (FIFO) to the device.
 
-    (*): See 'DvG_dev_Base__pyqt_lib.py' for details.
+    (*): See 'DvG_QDeviceIO.QDeviceIO' for details.
 
     Args:
         dev:
-            Reference to a 'DvG_dev_Arduino__fun_serial.Arduino' instance.
-        
-        (*) DAQ_update_interval_ms
-        (*) DAQ_function_to_run_each_update
-        (*) DAQ_critical_not_alive_count
+            Reference to a 'DvG_dev_Arduino__proto_serial.Arduino' instance.
+
+        (*) DAQ_function
+        (*) DAQ_interval_ms
         (*) DAQ_timer_type
-        
+        (*) critical_not_alive_count
+
     Main methods:
-        (*) start_thread_worker_DAQ(...)
-        (*) start_thread_worker_send(...)
-        (*) close_all_threads()
-        
+        (*) start(...)
+        (*) close()
+
         queued_write(...):
-            Write a message to the Arduino via the worker_send queue.
-        
-    Inner-class instances:
-        (*) worker_DAQ
-        (*) worker_send
-        
+            Write a message to the Arduino via the worker_jobs queue.
+
     Main data attributes:
-        (*) DAQ_update_counter
-        (*) obtained_DAQ_update_interval_ms
+        (*) update_counter_DAQ
+        (*) obtained_DAQ_interval_ms
         (*) obtained_DAQ_rate_Hz
 
     Signals:
         (*) signal_DAQ_updated()
         (*) signal_connection_lost()
     """
-    def __init__(self,
-                 dev: Arduino_functions.Arduino,
-                 DAQ_update_interval_ms=1000,
-                 DAQ_function_to_run_each_update=None,
-                 DAQ_critical_not_alive_count=3,
-                 DAQ_timer_type=QtCore.Qt.PreciseTimer,
-                 DAQ_trigger_by=Dev_Base_pyqt_lib.DAQ_trigger.CONTINUOUS,
-                 calc_DAQ_rate_every_N_iter=25,
-                 parent=None):
+
+    def __init__(
+        self,
+        dev: DvG_dev_Arduino__proto_serial.Arduino,
+        DAQ_trigger=DAQ_trigger.CONTINUOUS,
+        DAQ_function=None,
+        DAQ_interval_ms=1000,
+        DAQ_timer_type=QtCore.Qt.PreciseTimer,
+        critical_not_alive_count=3,
+        calc_DAQ_rate_every_N_iter=25,
+        parent=None,
+    ):
         super(Arduino_pyqt, self).__init__(parent=parent)
 
         self.attach_device(dev)
 
-        self.create_worker_DAQ(DAQ_update_interval_ms,
-                               DAQ_function_to_run_each_update,
-                               DAQ_critical_not_alive_count,
-                               DAQ_timer_type,
-                               DAQ_trigger_by,
-                               calc_DAQ_rate_every_N_iter=calc_DAQ_rate_every_N_iter,
-                               DEBUG=DEBUG_worker_DAQ)
+        self.create_worker_DAQ(
+            DAQ_trigger=DAQ_trigger,
+            DAQ_function=DAQ_function,
+            DAQ_interval_ms=DAQ_interval_ms,
+            DAQ_timer_type=DAQ_timer_type,
+            critical_not_alive_count=critical_not_alive_count,
+            calc_DAQ_rate_every_N_iter=calc_DAQ_rate_every_N_iter,
+            debug=DEBUG_worker_DAQ,
+        )
 
-        self.create_worker_send(DEBUG=DEBUG_worker_send)
+        self.create_worker_jobs(debug=DEBUG_worker_jobs)
 
     # --------------------------------------------------------------------------
     #   queued_write
@@ -97,6 +98,6 @@ class Arduino_pyqt(Dev_Base_pyqt_lib.Dev_Base_pyqt, QtCore.QObject):
 
     def queued_write(self, msg_str):
         """Send I/O operation 'write' with argument 'msg_str' to the Arduino via
-        the worker_send queue and process the queue.
+        the worker_jobs queue and process the queue.
         """
-        self.worker_send.queued_instruction(self.dev.write, msg_str)
+        self.send(self.dev.write, msg_str)
--- src/DvG_dev_Arduino__proto_serial.py
+++ src/DvG_dev_Arduino__proto_serial.py
@@ -24,7 +24,7 @@ See [DvG_SerialCommand](https://github.com/Dennis-van-Gils/DvG_SerialCommand).
 Classes:
     Arduino(...):
         Manages serial communication with an Arduino(-like) device.
-    
+
         Methods:
             close():
                 Close the serial connection.
@@ -41,15 +41,15 @@ Classes:
             query_ascii_values(...)
                 Write a string to the serial port and return the reply, parsed
                 into a list of floats.
-        
+
         Important member:
             ser: serial.Serial instance belonging to the Arduino
 """
-__author__      = "Dennis van Gils"
+__author__ = "Dennis van Gils"
 __authoremail__ = "vangils.dennis@gmail.com"
-__url__         = "https://github.com/Dennis-van-Gils/DvG_dev_Arduino"
-__date__        = "15-08-2019"
-__version__     = "1.0.2"
+__url__ = "https://github.com/Dennis-van-Gils/python-dvg-devices"
+__date__ = "01-07-2020"  # 0.0.1 was stamped 15-08-2019
+__version__ = "0.0.1"  # 0.0.1 corresponds to prototype 1.0.2
 
 import sys
 import serial
@@ -58,17 +58,24 @@ from pathlib import Path
 
 from DvG_debug_functions import print_fancy_traceback as pft
 
-class Arduino():
-    def __init__(self, name="Ard", baudrate=9600,
-                 read_timeout=1, write_timeout=1,
-                 read_term_char='\n', write_term_char='\n'):
+
+class Arduino:
+    def __init__(
+        self,
+        name="Ard",
+        baudrate=9600,
+        read_timeout=1,
+        write_timeout=1,
+        read_term_char="\n",
+        write_term_char="\n",
+    ):
         # Reference to the serial.Serial device instance when a connection has
         # been established
         self.ser = None
-        
+
         # Given name for display and debugging purposes
         self.name = name
-        
+
         # Response of self.query('id?') received from the Arduino.
         # Note that the Arduino should be programmed to respond to such a
         # query if you want the following functionality:
@@ -79,12 +86,12 @@ class Arduino():
         # or
         #   self.auto_connect(path_config, match_identity='your identity here')
         self.identity = None
-        
+
         # Serial communication settings
         self.baudrate = baudrate
-        self.read_timeout  = read_timeout
+        self.read_timeout = read_timeout
         self.write_timeout = write_timeout
-        self.read_term_char  = read_term_char
+        self.read_term_char = read_term_char
         self.write_term_char = write_term_char
 
         # Is the connection to the device alive?
@@ -99,24 +106,31 @@ class Arduino():
         """
         # Prevent Windows, thinking to be smart, from keeping the port open in
         # case the connection got lost
-        try: self.ser.cancel_read()
-        except: pass
-        try: self.ser.cancel_write()
-        except: pass
-    
-        try: self.ser.close()
-        except: pass
-        
+        try:
+            self.ser.cancel_read()
+        except:
+            pass
+        try:
+            self.ser.cancel_write()
+        except:
+            pass
+
+        try:
+            self.ser.close()
+        except:
+            pass
+
         self.is_alive = False
 
     # --------------------------------------------------------------------------
     #   connect_at_port
     # --------------------------------------------------------------------------
 
-    def connect_at_port(self, port_str, match_identity=None,
-                        print_trying_message=True):
+    def connect_at_port(
+        self, port_str, match_identity=None, print_trying_message=True
+    ):
         """Open the port at address 'port_str' and try to establish a
-        connection. Subsequently, an identity query is send to the device. 
+        connection. Subsequently, an identity query is send to the device.
         Optionally, if a 'match_identity' string is passed, only connections to
         devices with a matching identity response are accepted.
 
@@ -134,21 +148,23 @@ class Arduino():
         """
         self.is_alive = False
 
-        if match_identity == '': match_identity = None
+        if match_identity == "":
+            match_identity = None
         if print_trying_message:
             if match_identity is None:
                 print("Connect to Arduino")
             else:
-                print("Connect to Arduino with identity '%s'" %
-                      match_identity)
+                print("Connect to Arduino with identity '%s'" % match_identity)
 
-        print("  @ %-5s: " % port_str, end='')
+        print("  @ %-5s: " % port_str, end="")
         try:
             # Open the serial port
-            self.ser = serial.Serial(port=port_str,
-                                     baudrate=self.baudrate,
-                                     timeout=self.read_timeout,
-                                     write_timeout=self.write_timeout)
+            self.ser = serial.Serial(
+                port=port_str,
+                baudrate=self.baudrate,
+                timeout=self.read_timeout,
+                write_timeout=self.write_timeout,
+            )
         except (serial.SerialException):
             print("Could not open port")
             return False
@@ -161,13 +177,14 @@ class Arduino():
             [success, identity_str] = self.query("id?", timeout_warning_style=2)
         except:
             print("Identity query 'id?' failed")
-            if self.ser is not None: self.ser.close()
+            if self.ser is not None:
+                self.ser.close()
             self.is_alive = False
             return False
 
         if success:
             self.identity = identity_str
-            print("ID '%s' : " % identity_str, end='')
+            print("ID '%s' : " % identity_str, end="")
             if match_identity is None:
                 # Found any device
                 print("Success!")
@@ -187,7 +204,8 @@ class Arduino():
                 return False
 
         print("Wrong or no device")
-        if self.ser is not None: self.ser.close()
+        if self.ser is not None:
+            self.ser.close()
         self.is_alive = False
         return False
 
@@ -208,12 +226,15 @@ class Arduino():
 
         Returns: True if successful, False otherwise.
         """
-        if match_identity == '': match_identity = None
+        if match_identity == "":
+            match_identity = None
         if match_identity is None:
             print("Scanning ports for any Arduino")
         else:
-            print(("Scanning ports for an Arduino with "
-                   "identity '%s'") % match_identity)
+            print(
+                ("Scanning ports for an Arduino with " "identity '%s'")
+                % match_identity
+            )
 
         # Ports is a list of tuples
         ports = list(serial.tools.list_ports.comports())
@@ -232,7 +253,7 @@ class Arduino():
     #   auto_connect
     # --------------------------------------------------------------------------
 
-    def auto_connect(self, path_config='', match_identity=None):
+    def auto_connect(self, path_config="", match_identity=None):
         """
         """
         # Try to open the config file containing the port to open. Do not panic
@@ -250,7 +271,7 @@ class Arduino():
         # Check if we failed establishing a connection
         if not success:
             # Now scan over all ports and try to connect to the device
-            print('') # Terminal esthetics
+            print("")  # Terminal esthetics
             success = self.scan_ports(match_identity)
             if success:
                 # Store the result of a successful connection after a port scan
@@ -284,18 +305,20 @@ class Arduino():
         else:
             try:
                 self.ser.write((msg_str + self.write_term_char).encode())
-            except (serial.SerialTimeoutException,
-                    serial.SerialException) as err:
+            except (
+                serial.SerialTimeoutException,
+                serial.SerialException,
+            ) as err:
                 if timeout_warning_style == 1:
                     pft(err, 3)
                 elif timeout_warning_style == 2:
-                    raise(err)
+                    raise (err)
             except Exception as err:
                 pft(err, 3)
                 sys.exit(1)
             else:
                 success = True
-        
+
         return success
 
     # --------------------------------------------------------------------------
@@ -327,8 +350,10 @@ class Arduino():
         if self.write(msg_str, timeout_warning_style):
             try:
                 ans_bytes = self.ser.read_until(self.read_term_char.encode())
-            except (serial.SerialTimeoutException,
-                    serial.SerialException) as err:
+            except (
+                serial.SerialTimeoutException,
+                serial.SerialException,
+            ) as err:
                 # Note though: The Serial library does not throw an
                 # exception when it actually times out! We will check for
                 # zero received bytes as indication for timeout, later.
@@ -337,15 +362,15 @@ class Arduino():
                 pft(err, 3)
                 sys.exit(1)
             else:
-                if (len(ans_bytes) == 0):
+                if len(ans_bytes) == 0:
                     # Received 0 bytes, probably due to a timeout.
                     if timeout_warning_style == 1:
                         pft("Received 0 bytes. Read probably timed out.", 3)
                     elif timeout_warning_style == 2:
-                        raise(serial.SerialTimeoutException)
+                        raise (serial.SerialTimeoutException)
                 else:
                     try:
-                        ans_str = ans_bytes.decode('utf8').strip()
+                        ans_str = ans_bytes.decode("utf8").strip()
                     except UnicodeDecodeError as err:
                         # Print error and struggle on
                         pft(err, 3)
@@ -357,12 +382,12 @@ class Arduino():
 
         return [success, ans_str]
 
-    def query_ascii_values(self, msg_str="", separator='\t'):
+    def query_ascii_values(self, msg_str="", separator="\t"):
         """Send a message to the serial device and subsequently read the reply.
         Expects a reply from the Arduino in the form of an ASCII string
         containing a list of numeric values. These values will be parsed into a
         list of floats and returned.
-        
+
         Returns:
             success (bool):
                 True if successful, False otherwise.
@@ -371,8 +396,8 @@ class Arduino():
                 [None] if unsuccessful.
         """
         [success, ans_str] = self.query(msg_str)
-        
-        if success and not(ans_str == ''):
+
+        if success and not (ans_str == ""):
             try:
                 ans_floats = list(map(float, ans_str.split(separator)))
             except ValueError as err:
@@ -383,12 +408,14 @@ class Arduino():
                 sys.exit(1)
             else:
                 return [True, ans_floats]
-            
+
         return [False, []]
-    
+
+
 # ------------------------------------------------------------------------------
 #   read_port_config_file
 # ------------------------------------------------------------------------------
+
 
 def read_port_config_file(filepath):
     """Try to open the config textfile containing the port to open. Do not panic
@@ -409,13 +436,15 @@ def read_port_config_file(filepath):
                     port_str = f.readline().strip()
                 return port_str
             except:
-                pass    # Do not panic and remain silent
+                pass  # Do not panic and remain silent
 
     return None
+
 
 # ------------------------------------------------------------------------------
 #   write_port_config_file
 # ------------------------------------------------------------------------------
+
 
 def write_port_config_file(filepath, port_str):
     """Try to write the port name string to the config textfile. Do not panic if
@@ -426,7 +455,7 @@ def write_port_config_file(filepath, port_str):
             Path to the config file, e.g. Path("config/port.txt")
         port_str (string):
             Serial port string to save to file.
-            
+
     Returns: True when successful, False otherwise.
     """
     if isinstance(filepath, Path):
@@ -435,34 +464,36 @@ def write_port_config_file(filepath, port_str):
             try:
                 filepath.parent.mkdir()
             except:
-                pass    # Do not panic and remain silent
+                pass  # Do not panic and remain silent
 
         try:
             # Write the config file
             filepath.write_text(port_str)
         except:
-            pass        # Do not panic and remain silent
+            pass  # Do not panic and remain silent
         else:
             return True
 
     return False
 
+
 # ------------------------------------------------------------------------------
 #   Main: Will show a demo when run from the terminal
 # ------------------------------------------------------------------------------
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     ard = Arduino(name="Ard", baudrate=9600)
-    
-    ard.auto_connect(path_config=Path("last_used_port.txt"),
-                     match_identity="My Arduino")
-    #ard.scan_ports(match_identity="My Arduino")
-    #ard.scan_ports()
-    
-    if not(ard.is_alive):
+
+    # ard.auto_connect(
+    #     path_config=Path("last_used_port.txt"), match_identity="My Arduino"
+    # )
+    # ard.scan_ports(match_identity="My Arduino")
+    ard.scan_ports()
+
+    if not (ard.is_alive):
         sys.exit(1)
-    
+
     print(ard.query("?"))
-    print(ard.query_ascii_values("?", '\t'))
-    
+    print(ard.query_ascii_values("?", "\t"))
+
     ard.close()

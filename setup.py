f84989306e323d8b3b2afa8904f43f8144e7e701
Dennis-van-Gils <vangils.dennis@gmail.com>
2020-07-16 16:46:23 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -79,6 +79,7 @@ setup(
     install_requires=[
         "dvg-debug-functions~=2.0",
         "dvg-qdeviceio~=0.2.1",
+        "dvg-pid-controller~=2.0",
         "pyserial==3.4",
         "pyqt5~=5.12",
         "numpy~=1.15",

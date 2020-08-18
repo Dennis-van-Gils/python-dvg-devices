27670fb2cec2e233f914bfae99323420d7cb1150
Dennis-van-Gils <vangils.dennis@gmail.com>
2020-08-18 17:37:05 +0200

    Fully functioning version

---
+++ src/dvg_devices/Aim_TTi_PSU_demo.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+__author__ = "Dennis van Gils"
+__authoremail__ = "vangils.dennis@gmail.com"
+__url__ = "https://github.com/Dennis-van-Gils/python-dvg-devices"
+__date__ = "18-08-2020"
+__version__ = "0.2.1"
+
+import sys
+
+from PyQt5 import QtCore, QtGui
+from PyQt5 import QtWidgets as QtWid
+
+from dvg_pyqt_controls import SS_TEXTBOX_READ_ONLY, SS_GROUP
+from dvg_devices.Aim_TTi_PSU_protocol_RS232 import Aim_TTi_PSU
+from dvg_devices.Aim_TTi_PSU_qdev import Aim_TTi_PSU_qdev
+
+# Show debug info in terminal? Warning: Slow! Do not leave on unintentionally.
+DEBUG = False
+
+# ------------------------------------------------------------------------------
+#   MainWindow
+# ------------------------------------------------------------------------------
+
+
+class MainWindow(QtWid.QWidget):
+    def __init__(self, parent=None, **kwargs):
+        super().__init__(parent, **kwargs)
+
+        self.setGeometry(600, 120, 0, 0)
+        self.setWindowTitle("Aim TTi power supply control")
+
+        self.pbtn_exit = QtWid.QPushButton("Exit")
+        self.pbtn_exit.clicked.connect(self.close)
+        self.pbtn_exit.setMinimumHeight(30)
+
+        hbox = QtWid.QHBoxLayout()
+        hbox.addWidget(psu_qdev.grpb)
+        hbox.addWidget(self.pbtn_exit, alignment=QtCore.Qt.AlignTop)
+        hbox.addStretch(1)
+
+        vbox = QtWid.QVBoxLayout(self)
+        vbox.addLayout(hbox)
+        vbox.addStretch(1)
+
+
+# ------------------------------------------------------------------------------
+#   about_to_quit
+# ------------------------------------------------------------------------------
+
+
+def about_to_quit():
+    print("About to quit")
+    app.processEvents()
+    psu_qdev.quit()
+    psu.close()
+
+
+# ------------------------------------------------------------------------------
+#   Main
+# ------------------------------------------------------------------------------
+
+if __name__ == "__main__":
+    # Config file containing COM port address
+    PATH_PORT = "config/port_Aim_TTi_PSU.txt"
+
+    # The state of the PSU is polled with this time interval
+    DAQ_INTERVAL_MS = 200  # [ms]
+
+    # --------------------------------------------------------------------------
+    #   Connect to power supply
+    # --------------------------------------------------------------------------
+
+    psu = Aim_TTi_PSU(name="Aim TTi PSU")
+    if psu.auto_connect(filepath_last_known_port=PATH_PORT):
+        psu.begin()
+
+    # --------------------------------------------------------------------------
+    #   Create application
+    # --------------------------------------------------------------------------
+    QtCore.QThread.currentThread().setObjectName("MAIN")  # For DEBUG info
+
+    app = QtWid.QApplication(sys.argv)
+    app.setFont(QtGui.QFont("Arial", 9))
+    app.setStyleSheet(SS_TEXTBOX_READ_ONLY + SS_GROUP)
+    app.aboutToQuit.connect(about_to_quit)
+
+    # --------------------------------------------------------------------------
+    #   Set up communication threads for the PSU
+    # --------------------------------------------------------------------------
+
+    psu_qdev = Aim_TTi_PSU_qdev(
+        dev=psu, DAQ_interval_ms=DAQ_INTERVAL_MS, debug=DEBUG
+    )
+    psu_qdev.start()
+
+    # --------------------------------------------------------------------------
+    #   Start the main GUI event loop
+    # --------------------------------------------------------------------------
+
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
--- src/dvg_devices/Aim_TTi_PSU_protocol_RS232.py
+++ src/dvg_devices/Aim_TTi_PSU_protocol_RS232.py
@@ -9,7 +9,7 @@ Note:
 __author__ = "Dennis van Gils"
 __authoremail__ = "vangils.dennis@gmail.com"
 __url__ = "https://github.com/Dennis-van-Gils/python-dvg-devices"
-__date__ = "17-08-2020"
+__date__ = "18-08-2020"
 __version__ = "0.2.1"
 # pylint: disable=bare-except, broad-except, try-except-raise
 
@@ -31,10 +31,8 @@ class Aim_TTi_PSU(SerialDevice):
         """
 
         # fmt: off
-        V_source = 0        # Voltage to be sourced         [V]
-        I_source = 0        # Current to be sourced (limit) [A]
-        P_source = 0        # Power to be sourced, when PID controller is on [W]
-        ENA_PID = False     # Is the PID controller on the power output enabled?
+        V_source = 0            # Voltage to be sourced         [V]
+        I_source = 0            # Current to be sourced (limit) [A]
 
         V_meas = np.nan         # Measured output voltage [V]
         I_meas = np.nan         # Measured output current [A]
@@ -43,38 +41,23 @@ class Aim_TTi_PSU(SerialDevice):
         OVP_level  = np.nan     # Over-voltage protection level [V]
         OCP_level  = np.nan     # Over-current protection level [A]
         ENA_output = False      # Is power output enabled (by software)?
-        # fmt: on
-
-        """ NOT IMPLEMENTED YET
-        # The error string retreived from the error queue of the device. None
-        # indicates no error is left in the queue.
-        error = None
-
-        # This list of strings is provided to be able to store all errors from
-        # the device queue. This list is populated by calling 'query_error'
-        # until no error is left in the queue. This list can then be printed to
-        # screen or GUI and the user should 'acknowledge' the list, after which
-        # the list can be emptied (=[]) again.
-        all_errors = []
-        """
 
         # Limit Event Status Register (LSR)
-        # fmt: off
-        status_LSR_AUX_TRP = False  # Aux output trip                   , bit 7
-        status_LSR_AUX_CC = False   # Aux output constant-current mode  , bit 6
-        status_LSR_SN = False       # Sense trip                        , bit 5
-        status_LSR_OT = False       # Over-temperature trip             , bit 4
-        status_LSR_OCP = False      # Over-current trip                 , bit 3
-        status_LSR_OVP = False      # Over-voltage trip                 , bit 2
-        status_LSR_CC = False       # Constant-current mode             , bit 1
-        status_LSR_CV = False       # Constant-voltage mode             , bit 0
+        LSR_is_tripped = False    # True if any LSR tripped, ignoring mode
+        LSR_TRIP_AUX = False      # Aux output trip                   , bit 7
+        LSR_MODE_AUX_CC = False   # Aux output constant-current mode  , bit 6
+        LSR_TRIP_SENSE = False    # Sense trip                        , bit 5
+        LSR_TRIP_OTP = False      # Over-temperature trip             , bit 4
+        LSR_TRIP_OCP = False      # Over-current trip                 , bit 3
+        LSR_TRIP_OVP = False      # Over-voltage trip                 , bit 2
+        LSR_MODE_CC = False       # Constant-current mode             , bit 1
+        LSR_MODE_CV = False       # Constant-voltage mode             , bit 0
         # fmt: on
 
     class Config:
         # fmt: off
         V_source  = 10   # Voltage to be sourced         [V]
         I_source  = 0.5  # Current to be sourced (limit) [A]
-        P_source  = 0    # Power   to be sourced         [W]
         OVP_level = 12   # Over-voltage protection level [V]
         OCP_level = 1    # Over-current protection level [A]
         # fmt: on
@@ -153,21 +136,16 @@ class Aim_TTi_PSU(SerialDevice):
 
         Returns: True if successful, False otherwise.
         """
-        # Clear errors
-        # self.state.error = None
-        # self.state.all_errors = []
-
         if not self.is_alive:
             print("ERROR: Device is not connected yet or already closed.")
             return False
 
-        success = self.set_LSE(value=255)  # Report all limits and trips
+        success = self.set_LSE(value=255)  # Report on all trips and modes
         success &= self.query_OVP_level()
         success &= self.query_OCP_level()
         success &= self.query_V_source()
         success &= self.query_I_source()
         success &= self.query_LSR()
-        # self.query_all_errors_in_queue()
 
         return success
 
@@ -181,9 +159,6 @@ class Aim_TTi_PSU(SerialDevice):
         Returns: True if all messages were sent and received successfully,
             False otherwise.
         """
-        # Clear errors
-        # self.state.error = None
-        # self.state.all_errors = []
 
         if not self.is_alive:
             print("ERROR: Device is not connected yet or already closed.")
@@ -198,15 +173,13 @@ class Aim_TTi_PSU(SerialDevice):
         success &= self.set_OCP_level(self.config.OCP_level)
         success &= self.set_V_source(self.config.V_source)
         success &= self.set_I_source(self.config.I_source)
-        self.state.P_source = self.config.P_source
-        self.state.ENA_PID = False
 
+        success &= self.set_LSE(value=255)  # Report on all trips and modes
         success &= self.query_OVP_level()
         success &= self.query_OCP_level()
         success &= self.query_V_source()
         success &= self.query_I_source()
         success &= self.query_LSR()
-        # self.query_all_errors_in_queue()
 
         return success
 
@@ -291,7 +264,8 @@ class Aim_TTi_PSU(SerialDevice):
         return self.write_and_wait_for_opc("LSE%d %d" % (channel, value))
 
     def query_LSR(self, verbose: bool = False, channel: int = 1) -> bool:
-        """Query and parse the Limit Event Status Register (LSR).
+        """Query and parse the Limit Event Status Register (LSR). This holds
+        the protection trips and output modes.
 
         Returns: True if successful, False otherwise.
         """
@@ -303,24 +277,32 @@ class Aim_TTi_PSU(SerialDevice):
         if success:
             # fmt: off
             status_code = int(reply)
-            self.state.status_LSR_AUX_TRP = bool(status_code & 128)
-            self.state.status_LSR_AUX_CC  = bool(status_code & 64)
-            self.state.status_LSR_SN      = bool(status_code & 32)
-            self.state.status_LSR_OT      = bool(status_code & 16)
-            self.state.status_LSR_OCP     = bool(status_code & 8)
-            self.state.status_LSR_OVP     = bool(status_code & 4)
-            self.state.status_LSR_CC      = bool(status_code & 2)
-            self.state.status_LSR_CV      = bool(status_code & 1)
+            self.state.LSR_TRIP_AUX    = bool(status_code & 128)
+            self.state.LSR_MODE_AUX_CC = bool(status_code & 64)
+            self.state.LSR_TRIP_SENSE  = bool(status_code & 32)
+            self.state.LSR_TRIP_OTP    = bool(status_code & 16)
+            self.state.LSR_TRIP_OCP    = bool(status_code & 8)
+            self.state.LSR_TRIP_OVP    = bool(status_code & 4)
+            self.state.LSR_MODE_CC     = bool(status_code & 2)
+            self.state.LSR_MODE_CV     = bool(status_code & 1)
+
+            self.state.LSR_is_tripped = (
+                self.state.LSR_TRIP_AUX
+                or self.state.LSR_TRIP_SENSE
+                or self.state.LSR_TRIP_OTP
+                or self.state.LSR_TRIP_OCP
+                or self.state.LSR_TRIP_OVP
+            )
 
             if verbose:
-                if self.state.status_LSR_AUX_TRP: print("  AUX TRIP")
-                if self.state.status_LSR_AUX_CC : print("  AUX CC mode")
-                if self.state.status_LSR_SN     : print("  SENSE TRIP")
-                if self.state.status_LSR_OT     : print("  OT TRIP")
-                if self.state.status_LSR_OCP    : print("  OCP TRIP")
-                if self.state.status_LSR_OVP    : print("  OVP TRIP")
-                if self.state.status_LSR_CC     : print("  CC mode")
-                if self.state.status_LSR_CV     : print("  CV mode")
+                if self.state.LSR_TRIP_AUX    : print("  AUX TRIP")
+                if self.state.LSR_MODE_AUX_CC : print("  AUX CC mode")
+                if self.state.LSR_TRIP_SENSE  : print("  SENSE TRIP")
+                if self.state.LSR_TRIP_OTP    : print("  OVER-TEMPERATURE TRIP")
+                if self.state.LSR_TRIP_OCP    : print("  OVER-CURRENT TRIP")
+                if self.state.LSR_TRIP_OVP    : print("  OVER-VOLTAGE TRIP")
+                if self.state.LSR_MODE_CC     : print("  CC mode")
+                if self.state.LSR_MODE_CV     : print("  CV mode")
             # fmt: on
 
         return success
@@ -329,12 +311,12 @@ class Aim_TTi_PSU(SerialDevice):
     #   Protection related
     # --------------------------------------------------------------------------
 
-    def reset_trip(self) -> bool:
+    def reset_trips(self) -> bool:
         """Attempt to clear all trip conditions from all outputs.
 
         Returns: True if successful, False otherwise.
         """
-        return self.write_and_wait_for_opc("TRIPRST")
+        return self.write_and_wait_for_opc("triprst")
 
     def set_OVP_level(self, voltage_V, channel: int = 1) -> bool:
         """Returns: True if the message was sent successfully, False otherwise.
@@ -397,6 +379,15 @@ class Aim_TTi_PSU(SerialDevice):
     # --------------------------------------------------------------------------
     #   Output related
     # --------------------------------------------------------------------------
+
+    def reset_trips_and_turn_on(self, channel: int = 1) -> bool:
+        """Returns: True if the message was sent successfully, False otherwise.
+        """
+        if self.write_and_wait_for_opc("triprst;*opc;OP%d 1" % channel):
+            self.state.ENA_output = True
+            return True
+
+        return False
 
     def turn_on(self, channel: int = 1) -> bool:
         """Returns: True if the message was sent successfully, False otherwise.
@@ -557,12 +548,6 @@ class Aim_TTi_PSU(SerialDevice):
     def report(self, channel: int = 1):
         """Report to the terminal.
         """
-        # print("\nError")
-        # print(chr(0x2014) * 26)
-        # self.query_error(True)
-        # while not self.state.error is None:
-        #    self.query_error(True)
-
         self.query_LSR(verbose=True, channel=channel)
         self.query_ENA_output(channel=channel)
         self.query_OVP_level(channel=channel)
@@ -599,13 +584,10 @@ class Aim_TTi_PSU(SerialDevice):
 
     def read_config_file(self):
         """Try to open the config textfile containing:
-
-             V_source       # Voltage to be sourced [V]
-             I_source       # Current to be sourced [A]
-             P_source       # Power   to be sourced [W]
-             OVP_level      # Over-voltage protection level [V]
-             OCP_level      # Over-current protection level [A]
-
+            * V_source   # Voltage to be sourced         [V]
+            * I_source   # Current to be sourced (limit) [A]
+            * OVP_level  # Over-voltage protection level [V]
+            * OCP_level  # Over-current protection level [A]
         Do not panic if the file does not exist or cannot be read.
 
         Returns: True if successful, False otherwise.
@@ -615,7 +597,6 @@ class Aim_TTi_PSU(SerialDevice):
                 with self.path_config.open() as f:
                     self.config.V_source = float(f.readline().strip())
                     self.config.I_source = float(f.readline().strip())
-                    self.config.P_source = float(f.readline().strip())
                     self.config.OVP_level = float(f.readline().strip())
                     self.config.OCP_level = float(f.readline().strip())
 
@@ -631,13 +612,10 @@ class Aim_TTi_PSU(SerialDevice):
 
     def write_config_file(self):
         """Try to write the config textfile containing:
-
-             V_source       # Voltage to be sourced [V]
-             I_source       # Current to be sourced [A]
-             P_source       # Power   to be sourced [W]
-             OVP_level      # Over-voltage protection level [V]
-             OCP_level      # Over-current protection level [A]
-
+             * V_source   # Voltage to be sourced         [V]
+             * I_source   # Current to be sourced (limit) [A]
+             * OVP_level  # Over-voltage protection level [V]
+             * OCP_level  # Over-current protection level [A]
         Do not panic if the file does not exist or cannot be read.
 
         Returns: True if successful, False otherwise.
@@ -653,11 +631,10 @@ class Aim_TTi_PSU(SerialDevice):
         try:
             # Write the config file
             self.path_config.write_text(
-                "%.3f\n%.3f\n%.3f\n%.1f\n%.2f"
+                "%.3f\n%.3f\n%.1f\n%.2f"
                 % (
                     self.state.V_source,
                     self.state.I_source,
-                    self.state.P_source,
                     self.state.OVP_level,
                     self.state.OCP_level,
                 )

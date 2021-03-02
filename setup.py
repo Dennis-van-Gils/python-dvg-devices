936b8f5640da05f493c53f316c055a6780514853
Dennis van Gils <37263907+Dennis-van-Gils@users.noreply.github.com>
2021-03-02 17:21:54 +0100

    Bump version == 0.2.4

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(*names, **kwargs):
 
 setup(
     name="dvg-devices",
-    version="0.2.3",
+    version="0.2.4",
     license="MIT",
     description="Collection of I/O interfaces to communicate with microcontroller boards and laboratory devices, with optional PyQt5 multithread support and graphical user-interfaces.",
     long_description="%s\n%s"
